8f11a1c6259c1e28a7731a914f8906de3b95f478
kori-irrlicht <kori.irrlicht@gmail.com>
2016-06-13 12:45:32 +0200

    Init SDL2

--- src/BlackSnow.c
+++ src/BlackSnow.c
@@ -10,15 +10,51 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
 #include "gettext.h"
+
 #define _(String) gettext (String)
+
+bool bs_init();
+void bs_close();
 
 int main(void) {
 
-	setlocale (LC_ALL, "");
-	bindtextdomain (PACKAGE, LOCALEDIR);
-	textdomain (PACKAGE);
+    bs_init();
 
-	puts(_("Hello World")); /* prints Hello World */
-	return EXIT_SUCCESS;
+    puts(_("Hello World")); /* prints Hello World */
+
+    bs_close();
+
+    return EXIT_SUCCESS;
+}
+
+bool bs_init() {
+    if (setlocale(LC_ALL,
+                  "") == NULL) {
+        puts("setlocale failed");
+        return false;
+    }
+    if (bindtextdomain(PACKAGE, LOCALEDIR) == NULL) {
+        puts("bindtextdomain failed");
+        return false;
+    }
+    if (textdomain(PACKAGE) == NULL) {
+        puts("textdomain failed");
+        return false;
+    }
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL initialization failed! SDL_Error: %s\n",
+               SDL_GetError());
+        return true;
+    }
+
+    return true;
+}
+
+void bs_close() {
+    SDL_Quit();
 }
