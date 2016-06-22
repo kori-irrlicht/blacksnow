e1b9cb3df0b44f7be8016c036600f60d824384b4
kori-irrlicht <kori.irrlicht@gmail.com>
2016-06-22 09:02:35 +0200

    Add GetTexture

--- src/asset.c
+++ src/asset.c
@@ -18,3 +18,17 @@ struct AssetTree {
     struct AssetTree *rightChild;
 };
 
+static int assets_GetAsset(AssetTree * at, char *path, struct Asset *asset) {
+    return 0;
+}
+
+SDL_Texture *assets_GetTexture(AssetTree * at, char *path) {
+
+    struct Asset a = {.type = ASSET_TEXTURE,.path = path };
+    int code = assets_GetAsset(at, path, &a);
+    if (code == 0) {
+        return a.texture;
+    }
+    return NULL;
+
+}
